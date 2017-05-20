9eb8d5f2db13289f60407ddceeaa5633e00c9e60
autodidaddict <alothien@gmail.com>
2017-05-20 13:52:15 -0400

    First commit

---
+++ src/WorkDispatch.Monitor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace WorkDispatch.Monitor
+{
+    class Program
+    {
+        private static ConnectionMultiplexer redis;
+
+        static void Main(string[] args)
+        {
+            redis = ConnectionMultiplexer.Connect("127.0.0.1");
+            redis.PreserveAsyncOrder = false;
+
+            ISubscriber sub = redis.GetSubscriber();
+            Console.WriteLine("Monitoring batch progress, press [Enter] to stop.");
+
+            sub.Subscribe("workitem-succeeded", (channel, message) => {
+                 var ids = ((string)message).Split(':');
+                 Console.WriteLine($"Batch {ids[0]} item {ids[1]} succeeded.");
+            });
+            sub.Subscribe("workitem-failed", (channel, message) => {
+                 var ids = ((string)message).Split(':');
+                 Console.WriteLine($"Batch {ids[0]} item {ids[1]} FAILED.");
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/WorkDispatch.SampleConsole/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace WorkDispatch.SampleConsole
+{
+    class Program
+    {
+        private static ConnectionMultiplexer redis;
+
+        static void Main(string[] args)
+        {
+            redis = ConnectionMultiplexer.Connect("127.0.0.1");
+            redis.PreserveAsyncOrder = false;
+
+            IDatabase db = redis.GetDatabase();
+            long batchId = db.StringIncrement("id:batches");
+
+            var batchHash = new HashEntry[] {
+                new HashEntry("owner", "Kevin"),
+                new HashEntry("created", System.Environment.TickCount),
+                new HashEntry("status", "Created"),
+                new HashEntry("totalitems", 1000),
+                new HashEntry("items_complete", 0),
+                new HashEntry("items_failed", 0)
+            };
+
+            db.HashSet($"batch:{batchId}", batchHash);
+
+            for (int x = 0; x < 1000; x++)
+            {
+                long workItemId = db.StringIncrement("id:workitems");
+                db.SetAdd($"batch:{batchId}:workitems", workItemId);
+                var workItemEntries = new HashEntry[] {
+                    new HashEntry("status", "Created"),
+                    new HashEntry("input1", "A"),
+                    new HashEntry("input2", "B"),
+                    new HashEntry("timestamp", System.Environment.TickCount)
+                };
+                db.HashSet($"workitem:{workItemId}", workItemEntries);
+            }
+
+            Console.WriteLine("Created a batch.");
+
+            var backgroundTasks = new[] {
+                Task.Run( () => WorkBatch(db, batchId, 1, 200)),
+                Task.Run( () => WorkBatch(db, batchId, 201, 500)),
+                Task.Run( () => WorkBatch(db, batchId, 501, 1000))
+            };
+
+            Task.WaitAll(backgroundTasks);
+            Console.WriteLine("Completed all batches.");
+        }
+
+        static void WorkBatch(IDatabase db, long batchId, int start, int finish)
+        {
+            var sub = redis.GetSubscriber();
+            for (int i = start; i < finish + 1; i++)
+            {
+                Thread.Sleep(50);
+                HashEntry[] update;
+                string channel = "workitem-succeeded";
+                if (i % 5 == 0)
+                {
+                    channel = "workitem-failed";
+                    update = new HashEntry[] {
+                        new HashEntry("status", "Failed")
+                    };
+                }
+                else
+                {
+                    update = new HashEntry[] {
+                        new HashEntry("status", "Completed")
+                    };
+                }
+                db.HashSet($"workitem:{i}", update);
+                sub.Publish(channel, $"{batchId}:{i}");
+            }
+        }
+    }
+}
